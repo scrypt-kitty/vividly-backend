ab1183a8bd340bb737a42c77d73c5d6dbb18936f
jastronaut <jas@jasdelgado.com>
2020-11-25 12:27:25 -0500

    temporarily increase request size limits while testing image uploads

--- server.js
+++ server.js
@@ -13,7 +13,8 @@ const upload = require('./routes/v0/upload');
 const app = express();
 
 // Bodyparser Middleware
-app.use(express.json());
+app.use(express.json({ limit: '25mb' }));
+app.use(express.urlencoded({ limit: '25mb' }));
 
 // MongoDB config
 const db = config.get('mongoURI');
