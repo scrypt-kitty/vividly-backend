56fb622f739d582a0981ee7f5fc39a4571c46697
jastronaut <jas@jasdelgado.com>
2020-12-05 13:29:23 -0500

    add length limits and clean up stuff

--- routes/v0/users.js
+++ routes/v0/users.js
@@ -1,24 +1,48 @@
 const express = require('express');
 const bcrypt = require('bcryptjs');
 const router = express.Router();
-const config = require('config');
 const jwt = require('jsonwebtoken');
 const auth = require('../../middleware/auth');
 
 const { User } = require('../../models/User');
 const { Post } = require('../../models/Post');
+const { makeIdFriendly } = require('../../utils');
+
+const usernameRegex = /^[a-zA-Z0-9_]{4,15}$/;
+
+function isNameValid(name) {
+	// TODO: add more checks
+	return name.length > 0 && name.length < 51;
+}
+
+function isPasswordValid(password) {
+	// TODO: add more checks
+	return password.length > 7;
+}
 
 // @route   POST v0/users/register
 // @desc    Register a new User
 // @access  Public
 router.post('/register', async (req, res) => {
-	const { name, username, password, email } = req.body;
+	let { name, username, password, email } = req.body;
+	name = name.trim();
+	username = username.trim();
+	email = email.trim();
+
 	if (!name || !username || !password || !email) {
 		return res.status(400).json({ msg: 'missing fields', succcess: false });
 	}
 
-	try {
+	if (!usernameRegex.test(usernameRegex))
+		return res.status(400).json({ success: false, msg: 'username is invalid' });
 
+	if (!isNameValid(name))
+		return res.status(400).json({ success: false, msg: 'name is invalid' });
+
+	if (!isPasswordValid)
+		return res.status(400).json({ success: false, msg: 'password is too short' });
+
+	try {
 		const user = await User.findOne({ username }).lean();
 		if (user)
 			return res.status(400).json({ msg: 'user with username already exists', success: false });
@@ -33,7 +57,6 @@ router.post('/register', async (req, res) => {
 			password,
 			email
 		});
-
 
 		bcrypt.genSalt(10, (err, salt) => {
 			bcrypt.hash(newUser.password, salt, (err, hash) => {
@@ -87,9 +110,9 @@ router.get('/lookup/:username', auth, (req, res) => {
 		.then(user => {
 			if (!user)
 				throw 'error';
-			res.json(user);
+			res.json({ success: true, user });
 		})
-		.catch(err => res.status(404).json({ success: false }));
+		.catch(err => res.status(404).json({ success: false, msg: 'user not found' }));
 });
 
 
@@ -99,7 +122,6 @@ router.get('/lookup/:username', auth, (req, res) => {
 router.delete('/', auth, async (req, res) => {
 	const user = req.user;
 	try {
-
 		// remove user from friends lists of other users
 		const friends = user.friends;
 		await Promise.all(friends.map(async (f) => {
@@ -108,16 +130,15 @@ router.delete('/', auth, async (req, res) => {
 				const friendFriends = friend.friends;
 				friend.friends = friendFriends.filter(ff => ff.friendId !== user.id);
 				await friend.save();
-			} catch (e) {}
+			} catch (e) { }
 		}));
 
 		// remove all posts by user
-		await Post.deleteMany({ authorId: user.id});
+		await Post.deleteMany({ authorId: user.id });
 
-		await user.remove();
 		return res.status(200).json({ succcess: true });
 	} catch (e) {
-		return res.status(500).json({ msg: 'cannot delete user at this time.'});
+		return res.status(500).json({ success: false, msg: 'cannot delete user at this time.' });
 	}
 
 });
@@ -128,9 +149,10 @@ router.delete('/', auth, async (req, res) => {
 router.get('/self', auth, (req, res) => {
 	try {
 		const user = req.user;
+		makeIdFriendly(user);
 		return res.status(200).json({ user, success: true });
 	} catch (e) {
-		return res.status(500).json({ msg: 'could not sign in' });
+		return res.status(500).json({ success: false, msg: 'could not sign in' });
 	}
 });
 
